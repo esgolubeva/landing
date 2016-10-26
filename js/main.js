91fccb9fe42f60986b34a26770333efb40d754c6
Елена Голубева < >
2016-10-27 00:41:41 +0300

    added team accordion

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
+//accordion
+
 $(document).ready(function() {
-	console.log($('.section').length);
+	
+	$('.team-item__trigger').on('click', function(e) {
+		e.preventDefault();
+
+			var
+				$this = $(this),
+				container = $this.closest('.team-list'),
+				item = $this.closest('.team-item'),
+				items = container.find('.team-item');
+
+				console.log($this);
+				console.log(container);
+				console.log(item);
+				console.log(items);
+
+			if(!item.hasClass('team-item_active')) {
+				items.removeClass('team-item_active');
+				item.addClass('team-item_active');
+			} else {
+				item.removeClass('team-item_active');
+			}
+	})
+
 });
+
