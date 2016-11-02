d5616e89658885e7eaac0376c74021d23459f98d
Елена Голубева < >
2016-11-02 17:51:04 +0300

    fixed sendMail

--- index.php
+++ index.php
@@ -1,18 +1,16 @@
 <?php
 
 if (isset($_POST['name'])) {$name = $_POST['name'];}
-if (isset($_POST['phone'])) {$phone = $_POST['phone'];}
-if (isset($_POST['email'])) {$email = $_POST['email'];}
 
-$mail = "ya@ya.ru";
+$mail = "lena19948@yandex.ru";
 
 $headers = "Content-type: text/plain; charset = utf-8";
 
 $subject = "Тема письма";
 
-$message = "Тело письма - Заявка\nИмя: $name\nТелефон: $phone\nEmail: $email";
+$message = "Сообщение от пользователя: $name";
 
-$send = mail ($mail, $subject, $message, $headers);
+$send = mail($mail, $subject, $message, $headers);
 
 if ($send == 'true')
 {
